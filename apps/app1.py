2892ab8745b639f533886473d4ea1424c408eed8
acboulet <acboulet@outlook.com>
2020-04-09 14:51:21 -0600

    oligo_class.py functioning as real class

--- apps/app1.py
+++ apps/app1.py
@@ -9,51 +9,85 @@ sys.path.append('..')
 from app import app
 import style
 
-#primer = 'atcg'
+# primer = 'atcg'
 
-#mutations = 3
+# mutations = 3
 class oligo():
     """Functions for characterization of site-directed mutagenesis primers"""
     def __init__(self, primer, mutations):
-        self.primer = primer
-        self.mutations = mutations
+        self._primer = self.prep(primer)
+        self._mutations = mutations
 
-    def prep(primer):
+    def prep(self, primer):
+        """
+        Function will provide a string value in all lowercases while removing all spaces
+
+        :param primer: Input primer sequence in string format
+        :return: Primer in string format without spaces and all lowercase letters
+        """
         primer= ''.join(primer.split())
         primer= primer.lower()
-        primer= str(primer)
         return(primer)
 
-    def comp(primer):
+    def comp(self):
+        """
+        Function will calculate total number of GC bases
+
+        :param primer: Input primer sequence in string format
+        :return: Integer value of total number of GC bases
+        """
         GC= 0
-        for i in primer:
-            if i == 'g' or i =='c':
+        for i in self._primer:
+            if i in 'gc':
                 GC += 1
-            else:
-                GC += 0
         return GC
 
-    def F_lockdown(primer):
-        if primer[-1] == 'c' or primer[-1] == 'g':
+    def F_lockdown(self):
+        """
+        Function will provide boolean answer to whether primer ends in GC pair
+
+        :param primer: Input primer sequence in string format
+        :return: Boolean answer to whether primer terminates with GC
+        """
+        if self._primer[-1] in 'gc':
             return True
         else:
             return False
 
     def R_lockdown(primer):
-        if primer[0] == 'c' or primer[0] == 'g':
+        """
+        Function will provide boolean answer to whether primer begins with G or C
+
+        :param primer: Input primer sequence in string format
+        :return: Boolean answer to whether primer begins with G or C
+        """
+        if self._primer[0] in 'gc':
             return True
         else:
             return False
 
-    def content(primer):
-        gc = oligo.comp(primer)
-        size = len(primer)
+    def content(self):
+        """
+        Calculates percentage content of GC in primer
+
+        :param primer: Input primer sequence in string format
+        :return: Float value of GC%
+        """
+        gc = oligo.comp(self._primer)
+        size = len(self._primer)
         gc_content = round(gc / size * 100)
         return gc_content
 
 
-    def Tm_temp(primer, mutations):
-        Tm= 81.5 + ((0.41 * oligo.content(primer))) - (675 / (len(primer)) - (mutations))
+    def Tm_temp(self):
+        """
+        Calcualtes Tm using Strategene site-directed mutagenesis formula
+
+        :param primer: Input primer sequence in string format
+        :param mutations: Integer value representing total number of mutations to create
+        :return: Float value represent Tm (in C) of primer
+        """
+        Tm= 81.5 + ((0.41 * oligo.content(self._primer))) - (675 / (len(self._primer)) - (self._mutations))
         Tm = round(Tm, 1)
         return Tm
 
@@ -89,6 +123,9 @@ layout = html.Div([
         html.H2(id='primer_size'),
         ], style={'padding-left':'140px'})
 ])
+
+primer = oligo(primer, mutations)
+print(primer.prep())
 #Input print
 @app.callback(
     Output('primer-out', 'children'),
---
+++ apps/oligo_class.py
@@ -0,0 +1,94 @@
+
+class oligo():
+    """Functions for characterization of site-directed mutagenesis primers"""
+    def __init__(self, primer, mutations):
+        self._primer = self.prep(primer)
+        self._mutations = mutations
+
+    def prep(self, primer):
+        """
+        Function will provide a string value in all lowercases while removing all spaces
+
+        :param primer: Input primer sequence in string format
+        :return: Primer in string format without spaces and all lowercase letters
+        """
+        self._primer= ''.join(primer.split())
+        self._primer= self._primer.lower()
+        return(self._primer)
+
+    def comp(self):
+        """
+        Function will calculate total number of GC bases
+
+        :param primer: Input primer sequence in string format
+        :return: Integer value of total number of GC bases
+        """
+        GC= 0
+        for i in self._primer:
+            if i in 'gc':
+                GC += 1
+        return GC
+
+    def F_lockdown(self):
+        """
+        Function will provide boolean answer to whether primer ends in GC pair
+
+        :param primer: Input primer sequence in string format
+        :return: Boolean answer to whether primer terminates with GC
+        """
+        if self._primer[-1] in 'gc':
+            return True
+        else:
+            return False
+
+    def R_lockdown(self):
+        """
+        Function will provide boolean answer to whether primer begins with G or C
+
+        :param primer: Input primer sequence in string format
+        :return: Boolean answer to whether primer begins with G or C
+        """
+        if self._primer[0] in 'gc':
+            return True
+        else:
+            return False
+
+    def content(self):
+        """
+        Calculates percentage content of GC in primer
+
+        :param primer: Input primer sequence in string format
+        :return: Float value of GC%
+        """
+        # primer = self._primer
+        # print(primer)
+        gc = oligo(self._primer, self._mutations).comp()
+        size = len(self._primer)
+        gc_content = round(gc / size * 100)
+        return gc_content
+
+
+    def Tm_temp(self):
+        """
+        Calcualtes Tm using Strategene site-directed mutagenesis formula
+
+        :param primer: Input primer sequence in string format
+        :param mutations: Integer value representing total number of mutations to create
+        :return: Float value represent Tm (in C) of primer
+        """
+        gc_content = oligo(self._primer, self._mutations).content()
+        Tm= 81.5 + ((0.41 * gc_content) - ((675 / (len(self._primer))) - self._mutations))
+        Tm = round(Tm, 1)
+        return Tm
+
+if __name__ == "__main__":
+    primer = 'ATGgcgcgcgcgcgcgccc'
+    mutations = 3
+
+    seq = oligo(primer, mutations)
+    print(seq._primer)
+    print('Comp', seq.comp())
+    print('R', seq.R_lockdown())
+    print('F', seq.F_lockdown())
+    print('Content', seq.content())
+    print('TM', seq.Tm_temp())
