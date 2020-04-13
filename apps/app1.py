e92b364a8d4611307b899d63a72af434c96a0fee
acboulet <acboulet@outlook.com>
2020-04-13 16:41:42 -0600

    Oligo class was created seperate from app1 Successfully imports upon running index.py
    Stock file returns forbidden error still. Likely won't fix any time soon

--- apps/app1.py
+++ apps/app1.py
@@ -2,96 +2,11 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-from oligo_class import oligo
 
-import sys 
-sys.path.append('..')
 
 from app import app
+from apps.oligo_class import oligo
 import style
-
-# primer = 'atcg'
-
-# mutations = 3
-# class oligo():
-#     """Functions for characterization of site-directed mutagenesis primers"""
-#     def __init__(self, primer, mutations):
-#         self._primer = self.prep(primer)
-#         self._mutations = mutations
-
-#     def prep(self, primer):
-#         """
-#         Function will provide a string value in all lowercases while removing all spaces
-
-#         :param primer: Input primer sequence in string format
-#         :return: Primer in string format without spaces and all lowercase letters
-#         """
-#         primer= ''.join(primer.split())
-#         primer= primer.lower()
-#         return(primer)
-
-#     def comp(self):
-#         """
-#         Function will calculate total number of GC bases
-
-#         :param primer: Input primer sequence in string format
-#         :return: Integer value of total number of GC bases
-#         """
-#         GC= 0
-#         for i in self._primer:
-#             if i in 'gc':
-#                 GC += 1
-#         return GC
-
-#     def F_lockdown(self):
-#         """
-#         Function will provide boolean answer to whether primer ends in GC pair
-
-#         :param primer: Input primer sequence in string format
-#         :return: Boolean answer to whether primer terminates with GC
-#         """
-#         if self._primer[-1] in 'gc':
-#             return True
-#         else:
-#             return False
-
-#     def R_lockdown(primer):
-#         """
-#         Function will provide boolean answer to whether primer begins with G or C
-
-#         :param primer: Input primer sequence in string format
-#         :return: Boolean answer to whether primer begins with G or C
-#         """
-#         if self._primer[0] in 'gc':
-#             return True
-#         else:
-#             return False
-
-#     def content(self):
-#         """
-#         Calculates percentage content of GC in primer
-
-#         :param primer: Input primer sequence in string format
-#         :return: Float value of GC%
-#         """
-#         gc = oligo.comp(self._primer)
-#         size = len(self._primer)
-#         gc_content = round(gc / size * 100)
-#         return gc_content
-
-
-#     def Tm_temp(self):
-#         """
-#         Calcualtes Tm using Strategene site-directed mutagenesis formula
-
-#         :param primer: Input primer sequence in string format
-#         :param mutations: Integer value representing total number of mutations to create
-#         :return: Float value represent Tm (in C) of primer
-#         """
-#         Tm= 81.5 + ((0.41 * oligo.content(self._primer))) - (675 / (len(self._primer)) - (self._mutations))
-#         Tm = round(Tm, 1)
-#         return Tm
-
 
 layout = html.Div([
     html.Div([
