619b05740e4356b0c1b3bacdef7402be82a3775d
acboulet <acboulet@outlook.com>
2020-06-17 16:17:11 -0600

    Don't lose this commit!
    This is ready to push

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
 import dash
 
-
-#Pulling dash v2.0 css from chriddyp on codepen
-# Will try to make my own sometime in the future, but can adjust anything directly in the future.
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__,
-                external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 server = app.server
 app.config.suppress_callback_exceptions = True
+
+# import dash_core_components as dcc
+# import dash_html_components as html
+# from dash.dependencies import Input, Output
+
+# import style
+
+# from apps import home, app1#, app2
+
+# app.layout = html.Div([
+#     html.Div([
+#     html.H1('Aren\'s Data Science Portfolio', className='logo', style=style.H1_style),
+#     html.Nav(className = "nav nav-pills", style=style.Nav_style,
+#         children=[
+#             html.A('Home', className="home", href='/', style=style.A_style),
+#             html.A('SDM Calculator', className="nav-stocker", href='/app1', style=style.A_style),
+#             html.A('Stock chart', className="nav-DNA", href='/app2', style=style.A_style)]),
+#             ],style={'background-color':'#9c805e',
+#                         'height':'105px'}),
+#     dcc.Location(id='url', refresh=False),
+#     html.Div(id='page-content')
+# ])
+
+# @app.callback(Output('page-content', 'children'),
+#               [Input('url', 'pathname')])
+
+# def display_page(pathname):
+#     if pathname == '/app1':
+#         return app1.layout
+#     # elif pathname == '/app2':
+#     #      return app2.layout
+#     else:
+#         return home.layout
+
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
--- apps/home.py
+++ apps/home.py
@@ -3,10 +3,11 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 
+import style
 from app import app
 
 layout = html.Div([
-    html.H3('Data analysis portfolio for Aren Boulet',),
+    html.H2('Data analysis portfolio for Aren Boulet', style = style.H2_style),
     html.P("""My name is Aren Boulet, and I built this website to display a portfolio of my coding skillset. This website 
             and all subsequent data analysis was coded using Python."""),
 ], style={'padding-left':'120px'}
